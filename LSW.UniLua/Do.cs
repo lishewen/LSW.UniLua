a5548e02fc878b83a6a4cb8bc29cdef2ade7dc6b
算神 <lishewen@msn.com>
2019-07-30 07:53:00 +0800

    fix D_ReallocStack

--- LSW.UniLua/Do.cs
+++ LSW.UniLua/Do.cs
@@ -12,7 +12,7 @@ namespace LSW.UniLua
 	{
 		public ThreadStatus ErrCode { get; private set; }
 
-		public LuaRuntimeException( ThreadStatus errCode )
+		public LuaRuntimeException(ThreadStatus errCode)
 		{
 			ErrCode = errCode;
 		}
@@ -20,12 +20,12 @@ namespace LSW.UniLua
 
 	public partial class LuaState
 	{
-		internal void D_Throw( ThreadStatus errCode )
+		internal void D_Throw(ThreadStatus errCode)
 		{
-			throw new LuaRuntimeException( errCode );
+			throw new LuaRuntimeException(errCode);
 		}
 
-		private ThreadStatus D_RawRunProtected<T>( PFuncDelegate<T> func, ref T ud )
+		private ThreadStatus D_RawRunProtected<T>(PFuncDelegate<T> func, ref T ud)
 		{
 			int oldNumCSharpCalls = NumCSharpCalls;
 			ThreadStatus res = ThreadStatus.LUA_OK;
@@ -33,7 +33,7 @@ namespace LSW.UniLua
 			{
 				func(ref ud);
 			}
-			catch( LuaRuntimeException e )
+			catch (LuaRuntimeException e)
 			{
 				NumCSharpCalls = oldNumCSharpCalls;
 				res = e.ErrCode;
@@ -42,9 +42,9 @@ namespace LSW.UniLua
 			return res;
 		}
 
-		private void SetErrorObj( ThreadStatus errCode, StkId oldTop )
+		private void SetErrorObj(ThreadStatus errCode, StkId oldTop)
 		{
-			switch( errCode )
+			switch (errCode)
 			{
 				case ThreadStatus.LUA_ERRMEM: // memory error?
 					oldTop.V.SetSValue("not enough memory");
@@ -55,26 +55,26 @@ namespace LSW.UniLua
 					break;
 
 				default: // error message on current top
-					oldTop.V.SetObj(ref Stack[Top.Index-1].V);
+					oldTop.V.SetObj(ref Stack[Top.Index - 1].V);
 					break;
 			}
-			Top = Stack[oldTop.Index+1];
+			Top = Stack[oldTop.Index + 1];
 		}
 
-		private ThreadStatus D_PCall<T>( PFuncDelegate<T> func, ref T ud,
-			int oldTopIndex, int errFunc )
+		private ThreadStatus D_PCall<T>(PFuncDelegate<T> func, ref T ud,
+			int oldTopIndex, int errFunc)
 		{
-			int oldCIIndex 			= CI.Index;
-			bool oldAllowHook 		= AllowHook;
-			int oldNumNonYieldable	= NumNonYieldable;
-			int oldErrFunc			= ErrFunc;
+			int oldCIIndex = CI.Index;
+			bool oldAllowHook = AllowHook;
+			int oldNumNonYieldable = NumNonYieldable;
+			int oldErrFunc = ErrFunc;
 
 			ErrFunc = errFunc;
-			ThreadStatus status = D_RawRunProtected<T>( func, ref ud );
-			if( status != ThreadStatus.LUA_OK ) // an error occurred?
+			ThreadStatus status = D_RawRunProtected<T>(func, ref ud);
+			if (status != ThreadStatus.LUA_OK) // an error occurred?
 			{
-				F_Close( Stack[oldTopIndex] );
-				SetErrorObj( status, Stack[oldTopIndex] );
+				F_Close(Stack[oldTopIndex]);
+				SetErrorObj(status, Stack[oldTopIndex]);
 				CI = BaseCI[oldCIIndex];
 				AllowHook = oldAllowHook;
 				NumNonYieldable = oldNumNonYieldable;
@@ -83,22 +83,22 @@ namespace LSW.UniLua
 			return status;
 		}
 
-		private void D_Call( StkId func, int nResults, bool allowYield )
+		private void D_Call(StkId func, int nResults, bool allowYield)
 		{
-			if( ++NumCSharpCalls >= LuaLimits.LUAI_MAXCCALLS )
+			if (++NumCSharpCalls >= LuaLimits.LUAI_MAXCCALLS)
 			{
-				if( NumCSharpCalls == LuaLimits.LUAI_MAXCCALLS )
-					G_RunError( "CSharp Stack Overflow" );
-				else if( NumCSharpCalls >=
-						(LuaLimits.LUAI_MAXCCALLS + (LuaLimits.LUAI_MAXCCALLS>>3))
+				if (NumCSharpCalls == LuaLimits.LUAI_MAXCCALLS)
+					G_RunError("CSharp Stack Overflow");
+				else if (NumCSharpCalls >=
+						(LuaLimits.LUAI_MAXCCALLS + (LuaLimits.LUAI_MAXCCALLS >> 3))
 					)
-					D_Throw( ThreadStatus.LUA_ERRERR );
+					D_Throw(ThreadStatus.LUA_ERRERR);
 			}
-			if( !allowYield )
+			if (!allowYield)
 				NumNonYieldable++;
-			if( !D_PreCall( func, nResults ) ) // is a Lua function ?
+			if (!D_PreCall(func, nResults)) // is a Lua function ?
 				V_Execute();
-			if( !allowYield )
+			if (!allowYield)
 				NumNonYieldable--;
 			NumCSharpCalls--;
 		}
@@ -106,7 +106,7 @@ namespace LSW.UniLua
 		/// <summary>
 		/// return true if function has been executed
 		/// </summary>
-		private bool D_PreCall( StkId func, int nResults )
+		private bool D_PreCall(StkId func, int nResults)
 		{
 			// prepare for Lua call
 
@@ -115,16 +115,18 @@ namespace LSW.UniLua
 #endif
 
 			int funcIndex = func.Index;
-			if(!func.V.TtIsFunction()) {
+			if (!func.V.TtIsFunction())
+			{
 				// not a function
 				// retry with `function' tag method
-				func = tryFuncTM( func );
+				func = TryFuncTM(func);
 
 				// now it must be a function
-				return D_PreCall( func, nResults );
+				return D_PreCall(func, nResults);
 			}
 
-			if(func.V.ClIsLuaClosure()) {
+			if (func.V.ClIsLuaClosure())
+			{
 				var cl = func.V.ClLValue();
 				Utl.Assert(cl != null);
 				var p = cl.Proto;
@@ -134,18 +136,18 @@ namespace LSW.UniLua
 
 				// 补全参数
 				int n = (Top.Index - func.Index) - 1;
-				for( ; n<p.NumParams; ++n )
-					{ StkId.Inc(ref Top).V.SetNilValue(); }
+				for (; n < p.NumParams; ++n)
+				{ StkId.Inc(ref Top).V.SetNilValue(); }
 
-				int stackBase = (!p.IsVarArg) ? (func.Index + 1) : AdjustVarargs( p, n );
-				
+				int stackBase = (!p.IsVarArg) ? (func.Index + 1) : AdjustVarargs(p, n);
+
 				CI = ExtendCI();
 				CI.NumResults = nResults;
 				CI.FuncIndex = func.Index;
 				CI.BaseIndex = stackBase;
-				CI.TopIndex  = stackBase + p.MaxStackSize;
+				CI.TopIndex = stackBase + p.MaxStackSize;
 				Utl.Assert(CI.TopIndex <= StackLast);
-				CI.SavedPc = new InstructionPtr( p.Code, 0 );
+				CI.SavedPc = new InstructionPtr(p.Code, 0);
 				CI.CallStatus = CallStatus.CIST_LUA;
 
 				Top = Stack[CI.TopIndex];
@@ -153,7 +155,8 @@ namespace LSW.UniLua
 				return false;
 			}
 
-			if(func.V.ClIsCsClosure()) {
+			if (func.V.ClIsCsClosure())
+			{
 				var cscl = func.V.ClCsValue();
 				Utl.Assert(cscl != null);
 
@@ -167,10 +170,10 @@ namespace LSW.UniLua
 				CI.CallStatus = CallStatus.CIST_NONE;
 
 				// do the actual call
-				int n = cscl.F( this );
-				
+				int n = cscl.F(this);
+
 				// poscall
-				D_PosCall( Top.Index-n );
+				D_PosCall(Top.Index - n);
 
 				return true;
 			}
@@ -178,7 +181,7 @@ namespace LSW.UniLua
 			throw new System.NotImplementedException();
 		}
 
-		private int D_PosCall( int firstResultIndex )
+		private int D_PosCall(int firstResultIndex)
 		{
 			// TODO: hook
 			// be careful: CI may be changed after hook
@@ -192,10 +195,10 @@ namespace LSW.UniLua
 			ULDebug.Log( "[D] ==== PosCall wanted:" + wanted );
 #endif
 
-			CI = BaseCI[CI.Index-1];
+			CI = BaseCI[CI.Index - 1];
 
 			int i = wanted;
-			for( ; i!=0 && firstResultIndex < Top.Index; --i )
+			for (; i != 0 && firstResultIndex < Top.Index; --i)
 			{
 #if DEBUG_D_POS_CALL
 				ULDebug.Log( "[D] ==== PosCall assign lhs res:" + res );
@@ -203,7 +206,7 @@ namespace LSW.UniLua
 #endif
 				Stack[resIndex++].V.SetObj(ref Stack[firstResultIndex++].V);
 			}
-			while( i-- > 0 )
+			while (i-- > 0)
 			{
 #if DEBUG_D_POS_CALL
 				ULDebug.Log( "[D] ==== PosCall new LuaNil()" );
@@ -220,16 +223,19 @@ namespace LSW.UniLua
 		private CallInfo ExtendCI()
 		{
 			int newIndex = CI.Index + 1;
-			if(newIndex >= BaseCI.Length) {
-				int newLength = BaseCI.Length*2;
+			if (newIndex >= BaseCI.Length)
+			{
+				int newLength = BaseCI.Length * 2;
 				var newBaseCI = new CallInfo[newLength];
 				int i = 0;
-				while(i < BaseCI.Length) {
+				while (i < BaseCI.Length)
+				{
 					newBaseCI[i] = BaseCI[i];
 					newBaseCI[i].List = newBaseCI;
 					++i;
 				}
-				while(i < newLength) {
+				while (i < newLength)
+				{
 					var newCI = new CallInfo();
 					newBaseCI[i] = newCI;
 					newCI.List = newBaseCI;
@@ -240,12 +246,13 @@ namespace LSW.UniLua
 				CI = newBaseCI[CI.Index];
 				return newBaseCI[newIndex];
 			}
-			else {
+			else
+			{
 				return BaseCI[newIndex];
 			}
 		}
 
-		private int AdjustVarargs( LuaProto p, int actual )
+		private int AdjustVarargs(LuaProto p, int actual)
 		{
 			// 有 `...' 的情况
 			// 调用前: func (base)fixed-p1 fixed-p2 var-p1 var-p2 top
@@ -255,28 +262,28 @@ namespace LSW.UniLua
 			// func (base)fixed-p1 fixed-p2 (reserved...) top
 
 			int NumFixArgs = p.NumParams;
-			Utl.Assert( actual >= NumFixArgs, "AdjustVarargs (actual >= NumFixArgs) is false" );
+			Utl.Assert(actual >= NumFixArgs, "AdjustVarargs (actual >= NumFixArgs) is false");
 
-			int fixedArg = Top.Index - actual; 	// first fixed argument
-			int stackBase = Top.Index;		// final position of first argument
-			for( int i=stackBase; i<stackBase+NumFixArgs; ++i )
+			int fixedArg = Top.Index - actual;  // first fixed argument
+			int stackBase = Top.Index;      // final position of first argument
+			for (int i = stackBase; i < stackBase + NumFixArgs; ++i)
 			{
 				Stack[i].V.SetObj(ref Stack[fixedArg].V);
 				Stack[fixedArg++].V.SetNilValue();
 			}
-			Top = Stack[stackBase+NumFixArgs];
+			Top = Stack[stackBase + NumFixArgs];
 			return stackBase;
 		}
 
-		private StkId tryFuncTM( StkId func )
+		private StkId TryFuncTM(StkId func)
 		{
-			var tmObj = T_GetTMByObj( ref func.V, TMS.TM_CALL );
-			if(!tmObj.V.TtIsFunction())
-				G_TypeError( func, "call" );
+			var tmObj = T_GetTMByObj(ref func.V, TMS.TM_CALL);
+			if (!tmObj.V.TtIsFunction())
+				G_TypeError(func, "call");
 
 			// open a hole inside the stack at `func'
-			for(int i=Top.Index; i>func.Index; --i)
-				{ Stack[i].V.SetObj(ref Stack[i-1].V); }
+			for (int i = Top.Index; i > func.Index; --i)
+			{ Stack[i].V.SetObj(ref Stack[i - 1].V); }
 
 			IncrTop();
 			func.V.SetObj(ref tmObj.V);
@@ -285,7 +292,7 @@ namespace LSW.UniLua
 
 		private void D_CheckStack(int n)
 		{
-			if(StackLast - Top.Index <= n)
+			if (StackLast - Top.Index <= n)
 				D_GrowStack(n);
 			// TODO: FOR DEBUGGING
 			// else
@@ -298,16 +305,16 @@ namespace LSW.UniLua
 		private void D_GrowStack(int n)
 		{
 			int size = Stack.Length;
-			if(size > LuaConf.LUAI_MAXSTACK)
+			if (size > LuaConf.LUAI_MAXSTACK)
 				D_Throw(ThreadStatus.LUA_ERRERR);
 
 			int needed = Top.Index + n + LuaDef.EXTRA_STACK;
 			int newsize = 2 * size;
-			if(newsize > LuaConf.LUAI_MAXSTACK)
-				{ newsize = LuaConf.LUAI_MAXSTACK; }
-			if(newsize < needed)
-				{ newsize = needed; }
-			if(newsize > LuaConf.LUAI_MAXSTACK)
+			if (newsize > LuaConf.LUAI_MAXSTACK)
+			{ newsize = LuaConf.LUAI_MAXSTACK; }
+			if (newsize < needed)
+			{ newsize = needed; }
+			if (newsize > LuaConf.LUAI_MAXSTACK)
 			{
 				D_ReallocStack(ERRORSTACKSIZE);
 				G_RunError("stack overflow");
@@ -320,14 +327,16 @@ namespace LSW.UniLua
 
 		private void D_ReallocStack(int size)
 		{
-			Utl.Assert(size < LuaConf.LUAI_MAXSTACK || size == ERRORSTACKSIZE);
+			Utl.Assert(size <= LuaConf.LUAI_MAXSTACK || size == ERRORSTACKSIZE);
 			var newStack = new StkId[size];
 			int i = 0;
-			for( ; i<Stack.Length; ++i) {
+			for (; i < Stack.Length; ++i)
+			{
 				newStack[i] = Stack[i];
 				newStack[i].SetList(newStack);
 			}
-			for( ; i<size; ++i) {
+			for (; i < size; ++i)
+			{
 				newStack[i] = new StkId();
 				newStack[i].SetList(newStack);
 				newStack[i].SetIndex(i);
